ca1cea196bf78804b1cbf46735da09a0ca642899
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-10-13 12:03:47 +0200

    added navbar, links from the navbar, and some css

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'projects', to: 'projects#index'
   get 'projects/:id', to: 'projects#show', as: :project
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
