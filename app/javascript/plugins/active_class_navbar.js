b6e484223079ff993fc78bbfb9a963d355b2eb03
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-10-14 16:04:13 +0200

    added navigation facility to the navbar by highlightening the page u currently are on

--- app/javascript/plugins/active_class_navbar.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelectorAll('.item');
-menu.forEach((item) => {
-  item.addEventListener('click', (e) => {
-   e.currentTarget.classList.add('active');
-   console.log(e.currentTarget)
-  });
-})
-
