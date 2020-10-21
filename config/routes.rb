d89dd7e2b47527d4508a39170b5f3957967538b2
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-10-21 16:23:49 +0200

    added the route for the about section

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'projects/:id', to: 'projects#show', as: :project
   post "projects", to: "projects#create"
   get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
