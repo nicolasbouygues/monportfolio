3417cf97edd4cc496f3b34c1bc16e797a0623857
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-11-04 12:02:01 +0100

    added a navbar responsive for social networks as header

--- app/javascript/plugins/burger.js
+++ app/javascript/plugins/burger.js
@@ -1,5 +1,12 @@
 let mainNav = document.getElementById('link_jquery');
 let navBarToggle = document.getElementById('js-navbar-toggle');
+let burgerMenu = document.querySelector('.fa-bars');
+let body = document.querySelector('body');
+let socialBar = document.querySelector('.header-smart-nav');
+let navBar = document.querySelector('.navbar-myportfolio');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active-navbar');
+    burgerMenu.classList.toggle('disappear');
+    body.classList.toggle('ov-hidden')
+    socialBar.classList.toggle('visible')
   });
