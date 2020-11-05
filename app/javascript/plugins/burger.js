450733e2d3efedb5d7ba926a7423db5d707eb282
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-11-05 16:23:29 +0100

    added responsive css for show page

--- app/javascript/plugins/burger.js
+++ app/javascript/plugins/burger.js
@@ -4,10 +4,17 @@ let burgerMenu = document.querySelector('.fa-bars');
 let body = document.querySelector('body');
 let socialBar = document.querySelector('.header-smart-nav');
 let navBar = document.querySelector('.navbar-myportfolio');
+let close = document.querySelector('#x-nav');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active-navbar');
-    mainNav.classList.add('transform')
     burgerMenu.classList.toggle('disappear');
     body.classList.toggle('ov-hidden')
     socialBar.classList.toggle('visible')
   });
+
+close.addEventListener('click', function() {
+  mainNav.classList.remove('active-navbar')
+  burgerMenu.classList.remove('disappear');
+  body.classList.remove('ov-hidden')
+  socialBar.classList.remove('visible')
+})
